a1e58de81aab6240a9e216891e808c3d95c7ee6a
nataliatw <dewaayu0445@gmail.com>
2023-11-17 13:07:41 +0800

    Kumpul tugas Praktikum 9

---
+++ H071221021/Praktikum 9/Penjualan/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+      <i class="fa fa-bars"></i>
+    </button>
+
+    <h1 style="font-weight: bold; font-size: 24px; color:black">Natalia.</h1>
+
+    <a class="nav-link" style="color:royalblue" href="{{ route('dashboard') }}">
+      <span>Dashboard</span></a>
+  
+    <a class="nav-link" style="color: royalblue" href="{{ route('barang') }}">
+      <span>Barang</span></a>
+
+</nav>
